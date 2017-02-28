8a1162ce2527aaedd0a92e25a1e6d131313f0dd5
picorana <dibartolomeo.sara@gmail.com>
2017-02-28 14:27:06 +0100

    removing unused files

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -5,30 +5,44 @@ import random
 import numpy as np
 from collections import defaultdict
 
-json_file = open("./partial/inverted_subreddits.json", 'r')
 users_file = open("./partial/users.txt", 'r')
-defaults_file = open("./partial/defaults.json", 'r')
-outfile = open('data2.json', 'w')
-subs_dict = json.load(json_file)
-defaults_list = json.load(defaults_file)
+subs_dict = json.load(open("./partial/inverted_subreddits.json", 'r'))
+defaults_list = json.load(open("./partial/defaults.json", 'r'))
 
-n_clusters_per_level = 40
-kmeans_cycles = 2
-users_threshold = 50
+n_clusters_per_level = 50
+kmeans_cycles = 4
+users_threshold = 4
+subs_per_user_threshold = 100
+nodes_per_level = 100
 
-users = []
-for line in users_file:
-	users.append(line.split("\t")[0])
+def retrieve_inverted_subreddits(subs_per_user_threshold):
+
+	subs_dict = defaultdict(list)
+
+	for line in users_file:
+		user = line.strip().split("\t")[0]
+		if len(line.strip().split("\t"))==1: continue
+		sublist = line.strip().split("\t")[1].split(" ")
+		if len(sublist) < subs_per_user_threshold: continue
+		for sub in sublist:
+			sub = sub.split("::")[0]
+			subs_dict[sub].append(user)
+	return subs_dict
 
 
-def kmeans (this_subs_dict):
+def kmeans (this_subs_dict, depth):
+
+	print len(this_subs_dict)
 
 	centers = []
 	clusters = {}
 	cycle = 0
 
-	for i in range(n_clusters_per_level):
+	for i in range(n_clusters_per_level - depth*5):
 		centers.append(random.choice(this_subs_dict.keys()))
+	#if depth == 0: centers = defaults_list
+	
+	print centers
 
 	while (cycle < kmeans_cycles):
 
@@ -51,43 +65,14 @@ def kmeans (this_subs_dict):
 				if similarity >= max_similarity:
 					max_similarity = similarity
 					candidate = center
+			if candidate == "": candidate = random.choice(centers)
 			clusters[candidate].append(sub)
 
 			count+=1
-			print count,
+			if count%100==0: print count
 
 		pprint.pprint(clusters)
 
-		"""
-		centers = []
-		for item in clusters:
-			
-			avg = defaultdict(float)
-			num_of_users_not_zero = 0
-			for sub in clusters[item]:
-				for user in this_subs_dict[sub]:
-					avg[user] += 1
-			for user in avg:
-				avg[user] = int(round(avg[user]/len(clusters[item])))
-				if avg[user] != 0: num_of_users_not_zero += 1
-
-			candidate = ""
-			max_similarity = 0
-			for sub in clusters[item]:
-				size_of_intersection = 0
-				for user in avg:
-					if avg[user] != 0 and user in this_subs_dict[sub]:
-						size_of_intersection += 1
-				divisor = math.sqrt(num_of_users_not_zero * len(this_subs_dict[sub]))
-				if divisor == 0:
-					similarity = 0
-				else:
-					similarity = size_of_intersection / divisor
-				if similarity >= max_similarity:
-					max_similarity = similarity
-					candidate = sub
-			centers.append(candidate)
-		"""
 		centers = []
 
 		for item in clusters:
@@ -106,25 +91,46 @@ def kmeans (this_subs_dict):
 	return clusters	
 	
 
-def fill_tree(last_node, this_subs_dict):
-	first_cluster = kmeans(this_subs_dict)
+def fill_tree(last_node, this_subs_dict, depth):
+	first_cluster = kmeans(this_subs_dict, depth)
 	pprint.pprint(first_cluster)
+
+	list_of_separated_nodes = []
+
 	for c in first_cluster:
-		cluster_dict = {}
-		cluster_dict['name'] = c
-		cluster_dict['children'] = []
-		if len(first_cluster[c]) < 100:
+		cluster_dict = {'name':c, 'children' = []}
+		
+		if len(first_cluster[c]) < 10:
 			for sub in first_cluster[c]:
-				subdict = {}
-				subdict['name'] = sub
-				subdict['size'] = len(subs_dict[sub])
-				cluster_dict['children'].append(subdict)
+				subdict = {'name':sub, 'size':len(subs_dict[sub])}
+				list_of_separated_nodes.append(sub)
+
+		elif len(first_cluster[c]) < nodes_per_level:
+			for sub in first_cluster[c]:
+				cluster_dict['children'].append({ 'name' : sub, 'size' : len(subs_dict[sub]) })
+
 		else:
 			new_subs_dict = {}
 			for sub in first_cluster[c]:
 				new_subs_dict[sub] = subs_dict[sub]
-			fill_tree(cluster_dict, new_subs_dict)
+			fill_tree(cluster_dict, new_subs_dict, depth+1)
+
 		last_node['children'].append(cluster_dict)
+
+	if len(list_of_separated_nodes) > 100 and depth < 8:
+		new_subs_dict = {}
+		for sub in list_of_separated_nodes:
+			new_subs_dict[sub] = subs_dict[sub]
+		print len(list_of_separated_nodes)
+		print len(new_subs_dict)
+		if len(new_subs_dict)>5: fill_tree(last_node, new_subs_dict, depth+1)
+	else:
+		for sub in list_of_separated_nodes:
+			last_node['children'].append({ 'name' : sub, 'size' : len(subs_dict[sub]) })
+
+print len(subs_dict)
+subs_dict = retrieve_inverted_subreddits(subs_per_user_threshold)
+print len(subs_dict)
 
 tree = {}
 tree['name'] = 'reddit'
@@ -133,12 +139,13 @@ tree['children'] = []
 this_subs_dict = {}
 
 for sub in subs_dict:
-	if sub in defaults_list: continue
-	if len(subs_dict[sub]) > users_threshold: this_subs_dict[sub] = subs_dict[sub]
+	#if sub in defaults_list: continue
+	if len(subs_dict[sub]) > users_threshold and len(subs_dict[sub])<3000: this_subs_dict[sub] = subs_dict[sub]
+	if len(subs_dict[sub]) > 3000: print len(subs_dict[sub])
 
-fill_tree(tree, this_subs_dict)
+fill_tree(tree, this_subs_dict, depth=0)
 
-json.dump(tree, outfile, indent=4)
+json.dump(tree, open('data2.json', 'w'), indent=4)
 
 
 
--- old/twitterprova.py
+++
@@ -1,9 +0,0 @@
-from pattern.web    import Twitter, plaintext
-from pattern.en     import tag
-from pattern.vector import KNN, count
-
-twitter = Twitter(language='en')
-
-
-for tweet in twitter.search('"more important than"'):
-   print plaintext(tweet.text)
